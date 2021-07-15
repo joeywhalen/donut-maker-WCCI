c5b514c54dea5142d74b7ffdd07e50aa937657af
Joey Whalen <jtktwhalen@gmail.com>
2021-07-15 16:10:58 -0400

    Still working on About the company link to produce a popup displaying game info.

--- src/js/nav-menu.js
+++ src/js/nav-menu.js
@@ -1,6 +1,6 @@
 
 
 function infoPopupButton() {
-    const infoPopup = document.getElementById("infoPopup");
+    const headernavopup = document.getElementById("infoPopup");
     popup.classList.toggle("show");
 }
