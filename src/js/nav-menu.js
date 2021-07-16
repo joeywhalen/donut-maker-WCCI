867ea782bfb3f9dc800151e9e7d42aa7f2002393
Joey Whalen <jtktwhalen@gmail.com>
2021-07-16 14:42:45 -0400

    Working on linking html and js.

--- src/js/nav-menu.js
+++ src/js/nav-menu.js
@@ -4,3 +4,23 @@ function infoPopupButton() {
     const headernavopup = document.getElementById("infoPopup");
     headernavpopup.classList.toggle("show");
 }
+
+function modalPopupButton() {
+    var modal = document.getElementById("meetBakerModal");
+    var btn = document.getElementById("meetBakerLink");
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
