685af0ecaa1d27fdb6fee2b177892b6d693bfbc1
Joey Whalen <jtktwhalen@gmail.com>
2021-07-17 14:32:42 -0400

    Brownie count, Auto Clicker count and cost, and Brownie Multiplier count and cost are all displaying but need work.

--- src/js/app.js
+++ src/js/app.js
@@ -4,39 +4,38 @@ const newBrownie = new BrownieMaker();
 
 const brownieButton = document.querySelector(".brownie-button");
 const brownieCount = document.querySelector(".click-count");
-brownieCount.innerHTML = newBrownie.clickCount;
 
 const purchaseAutoClickerButton = document.querySelector(".purchase-auto-clicker-button");
 const autoClickerCount = document.querySelector(".auto-clicker-count");
+const autoClickerCost = document.querySelector(".auto-clicker-cost");
 
 const purchaseBrownieMultiplierButton = document.querySelector(".purchase-brownie-multiplier-button");
 const brownieMultiplierCount = document.querySelector(".brownie-multiplier-count");
+const brownieMultiplierCost = document.querySelector(".brownie-multiplier-cost");
+
 
 brownieButton.addEventListener("click", () => {
     newBrownie.recordClick();
     console.log(newBrownie.clickCount);
-    brownieCount.innerHTML = newBrownie.brownieCount();
-    // document.querySelector(".click-count").innerHTML = newBrownie.clickCount;
+    brownieCount.innerHTML = newBrownie.clickCount;
 });
 
-// const updateBrownieCount = function (newBrownie) {
-//     newBrownie.brownieCount();
-//     newBrownie.clickCount;
-//     brownieCount.innerHTML = 5;
-//     brownieCount.innerHTML = newBrownie.clickCount;
-//     brownieCount.innerText = newBrownie.clickCount;
-// }
-
-const updBrownieCount = newBrownie.brownieCount();
+const updateBrownieCount = function (newBrownie) {
+    brownieCount.innerHTML = newBrownie.brownieCount();
+};
 
 purchaseAutoClickerButton.addEventListener("click", () => {
     newBrownie.purchaseAutoClicker();
     console.log(newBrownie.autoClickerCount);
+    autoClickerCount.innerHTML = newBrownie.autoClickerCount;
     console.log(newBrownie.autoClickerCost);
+    autoClickerCost.innerHTML = newBrownie.autoClickerCost;
 });
 
 purchaseBrownieMultiplierButton.addEventListener("click", () => {
     newBrownie.purchaseBrownieMultiplier();
     console.log(newBrownie.brownieMultiplierCount);
+    brownieMultiplierCount.innerHTML = newBrownie.brownieMultiplierCount;
     console.log(newBrownie.brownieMultiplierCost);
+    brownieMultiplierCost.innerHTML = newBrownie.brownieMultiplierCost;
 })
