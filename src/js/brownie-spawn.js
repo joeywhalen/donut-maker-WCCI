c85b83b2c9fbc418cf01d847e3531573cc200950
Joey Whalen <jtktwhalen@gmail.com>
2021-07-18 19:56:11 -0400

    Working on functionality for Brownie Spawn.

--- src/js/brownie-spawn.js
+++ src/js/brownie-spawn.js
@@ -33,13 +33,13 @@ class Particle{
     }
     draw(){
         context.globalAlpha = 1;
-        context.drawImage(donut,this.x, this.y, 20, 20);
+        context.drawImage(brownie,this.x, this.y, 20, 20);
     }
     update(i){
         this.y -= this.yMomentum;
         this.x -= this.xMomentum;
         this.yMomentum -= .2;
-        if(this.y - donut.height > canvas.height || this.x > canvas.width || this.x < 0 - donut.width){ // if donut is off the screen, remove it from the array
+        if(this.y - brownie.height > canvas.height || this.x > canvas.width || this.x < 0 - brownie.width){ // if brownie is off the screen, remove it from the array
             particleArray.splice(i, 1);
         } else {
             this.draw();
@@ -47,3 +47,31 @@ class Particle{
         console.log(particleArray.length);
     }
 }
+
+class BurstParticle{
+    constructor(){
+        this.x = Math.random() * canvas.width;
+        this.y = randomIntFromInterval(-0, 0);
+        this.yMomentum = randomIntFromInterval(-5,15) * Math.random();
+        this.xMomentum = randomIntFromInterval(-10, 10) * Math.random(); 
+        this.opacity = 1;
+    }
+    draw(){
+        context.globalAlpha = this.opacity;
+        this.opacity -= .01;
+        this.opacity = Math.max(0, this.opacity);
+        context.globalCompositeOperation = "destination-over";
+        context.drawImage(altBrownie, this.x, this.y, 20, 20)
+    }
+    update(i){
+        this.y -= this.yMomentum;
+        this.yMomentum -= Math.random()*.2;
+        this.x += this.xMomentum;
+        this.draw();
+        if(this.y - brownie.height - 100 > canvas.height || this.opacity == 0 || this.x > canvas.width || this.x < 0 - brownie.width){
+            //console.log("Off Screen!");
+            particleArray.splice(i, 1)
+        }
+    }
+}
+
